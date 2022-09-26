b3fe9547342806b69984e07552975b7d0d206786
mazylol <landonp95@gmail.com>
2022-09-26 16:27:48 -0400

    transitions & test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import Navbar from '../components/Navbar.svelte';
 </script>
 
-<div class="min-h-screen bg-[#5865F2] static">
-    <Navbar class="sticky top-o z-50" />
-    <slot class="relative" />
+<div class="min-h-screen bg-[#5865F2]">
+    <Navbar />
+    <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,36 @@
 <div class="w-full">
-    <div class="text-center mx-auto mt-32 text-gray-200">
+    <div class="text-center mx-auto mt-[25vh] text-gray-200">
         <div class="font-bold text-9xl flex flex-row justify-center gap-4">
             <p>D</p>
-            <p class="translate-y-4 hover:-translate-y-4 mx-2 my-2 px-1 py-1">i</p>
-            <p class="-translate-y-6 hover:translate-y-6 mx-2 my-2 px-1 py-1">s</p>
-            <p class="translate-y-4 hover:-translate-y-4 mx-2 my-2 px-1 py-2">c</p>
-            <p class="-translate-y-6 hover:translate-y-6 mx-2 my-2 px-1 py-1">o</p>
+            <p
+                class="translate-y-4 hover:-translate-y-4 mx-2 my-2 px-1 py-1 transition-all duration-100"
+            >
+                i
+            </p>
+            <p
+                class="-translate-y-6 hover:translate-y-6 mx-2 my-2 px-1 py-1 transition-all duration-100"
+            >
+                s
+            </p>
+            <p
+                class="translate-y-4 hover:-translate-y-4 mx-2 my-2 px-1 py-1 transition-all duration-100"
+            >
+                c
+            </p>
+            <p
+                class="-translate-y-6 hover:translate-y-6 mx-2 my-2 px-1 py-1 transition-all duration-100"
+            >
+                o
+            </p>
             <p>d</p>
-            <p class="translate-y-9 rotate-12 ml-2 hover:rotate-45 mx-2 my-2 px-1 py-1">e</p>
+            <p
+                class="translate-y-9 rotate-12 ml-2 hover:rotate-45 mx-2 my-2 px-1 py-1 transition-all duration-100"
+            >
+                e
+            </p>
         </div>
+    </div>
+    <div class="bg-zinc-800 mt-[55vh]">
+        <p class="text-center text-5xl text-gray-200 pt-2 font-bold">About The Bot</p>
     </div>
 </div>

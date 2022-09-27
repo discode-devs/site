58b4b608864d2996eb2423d5a5bec352988c92a3
mazylol <landonp95@gmail.com>
2022-09-26 22:51:54 -0400

    more stuffs

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,18 +1,25 @@
 <div class="w-full pt-4 top-0 sticky z-50">
-    <div class="bg-zinc-700 max-w-xl rounded-full mx-auto h-12">
+    <div class="bg-zinc-700 max-w-2xl rounded-full mx-auto h-12">
         <div class="flex flex-row text-3xl text-gray-200 font-bold pt-1.5 justify-center">
             <a href="/" class="navlink-hover px-1 mx-4">
-                <p class="">Home</p>
+                <p>Home</p>
             </a>
             <a href="/blog" class="navlink-hover px-1 mx-4">
-                <p class="">Blog</p>
+                <p>Blog</p>
             </a>
             <a
                 href="https://github.com/discode-devs/rewrite"
                 target="_blank"
                 class="navlink-hover px-1 mx-4"
             >
-                <p class="">Source</p>
+                <p>Source</p>
+            </a>
+            <a
+                href="https://discord.com/api/oauth2/authorize?client_id=937476981321240626&permissions=139586825280&scope=bot%20applications.commands"
+                target="_blank"
+                class="navlink-hover px-1 mx-4"
+            >
+                <p>Invite</p>
             </a>
         </div>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,13 @@
 <div class="w-full">
     <div class="text-center mx-auto mt-[32.5vh] text-gray-200">
-        <div class="font-bold text-9xl flex flex-row justify-center gap-4">
-            <p
-                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                D
-            </p>
-            <p
-                class="translate-y-1 hover:-translate-y-1 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                i
-            </p>
-            <p
-                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                s
-            </p>
-            <p
-                class="translate-y-1 hover:-translate-y-1 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                c
-            </p>
-            <p
-                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                o
-            </p>
-            <p
-                class="translate-y-1 hover:-translate-y-1 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                d
-            </p>
-            <p
-                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
-            >
-                e
-            </p>
+        <div class="font-bold text-9xl flex flex-row justify-center gap-2">
+            <p class="funtext funtext-hoverdown">D</p>
+            <p class="funtext funtext-hoverup">i</p>
+            <p class="funtext funtext-hoverdown">s</p>
+            <p class="funtext funtext-hoverup">c</p>
+            <p class="funtext funtext-hoverdown">o</p>
+            <p class="funtext funtext-hoverup">d</p>
+            <p class="funtext funtext-hoverdown">e</p>
         </div>
-    </div>
-    <div class="bg-zinc-800 mt-[55vh]">
-        <p class="text-center text-5xl text-gray-200 pt-2 font-bold">About The Bot</p>
     </div>
 </div>

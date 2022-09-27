7c2dacf78dc37e4523a51f56d36128d54b5a560d
mazylol <landonp95@gmail.com>
2022-09-26 21:25:14 -0400

    lool

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,16 +1,16 @@
 <div class="w-full pt-4 top-0 sticky z-50">
     <div class="bg-zinc-700 max-w-xl rounded-full mx-auto h-12">
-        <div class="flex flex-row text-3xl text-gray-200 font-bold py-2 justify-center">
-            <a href="/" class="link-hover px-1 mx-4">
+        <div class="flex flex-row text-3xl text-gray-200 font-bold pt-1.5 justify-center">
+            <a href="/" class="navlink-hover px-1 mx-4">
                 <p class="">Home</p>
             </a>
-            <a href="/blog" class="link-hover px-1 mx-4">
+            <a href="/blog" class="navlink-hover px-1 mx-4">
                 <p class="">Blog</p>
             </a>
             <a
                 href="https://github.com/discode-devs/rewrite"
                 target="_blank"
-                class="link-hover px-1 mx-4"
+                class="navlink-hover px-1 mx-4"
             >
                 <p class="">Source</p>
             </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,38 @@
 <div class="w-full">
-    <div class="text-center mx-auto mt-[25vh] text-gray-200">
+    <div class="text-center mx-auto mt-[32.5vh] text-gray-200">
         <div class="font-bold text-9xl flex flex-row justify-center gap-4">
-            <p>D</p>
             <p
-                class="translate-y-4 hover:-translate-y-4 mx-2 my-2 px-1 py-1 transition-all duration-100"
+                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
+            >
+                D
+            </p>
+            <p
+                class="translate-y-1 hover:-translate-y-1 mx-1 my-1 px-1 py-1 transition-all duration-100"
             >
                 i
             </p>
             <p
-                class="-translate-y-6 hover:translate-y-6 mx-2 my-2 px-1 py-1 transition-all duration-100"
+                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
             >
                 s
             </p>
             <p
-                class="translate-y-4 hover:-translate-y-4 mx-2 my-2 px-1 py-1 transition-all duration-100"
+                class="translate-y-1 hover:-translate-y-1 mx-1 my-1 px-1 py-1 transition-all duration-100"
             >
                 c
             </p>
             <p
-                class="-translate-y-6 hover:translate-y-6 mx-2 my-2 px-1 py-1 transition-all duration-100"
+                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
             >
                 o
             </p>
-            <p>d</p>
             <p
-                class="translate-y-9 rotate-12 ml-2 hover:rotate-45 mx-2 my-2 px-1 py-1 transition-all duration-100"
+                class="translate-y-1 hover:-translate-y-1 mx-1 my-1 px-1 py-1 transition-all duration-100"
+            >
+                d
+            </p>
+            <p
+                class="-translate-y-2 hover:translate-y-2 mx-1 my-1 px-1 py-1 transition-all duration-100"
             >
                 e
             </p>
